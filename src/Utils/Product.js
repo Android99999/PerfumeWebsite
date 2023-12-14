6965c5960543d79d685a0730eb848a0b8da28103
McBaque <mcbaque01@gmail.com>
2023-12-15 01:56:53 +0800

    Adding Some Products

--- src/Utils/Product.js
+++ src/Utils/Product.js
@@ -3,6 +3,16 @@ import Product2 from "../Assets/Product2.png"
 import Product3 from "../Assets/Product3.png"
 import Product4 from "../Assets/Product4.png"
 import Product5 from "../Assets/Product5.png"
+import Product6 from "../Assets/Product6.png"
+import Product7 from "../Assets/Product7.png"
+import Product8 from "../Assets/Product8.png"
+import Product9 from "../Assets/Product9.png"
+import Product10 from "../Assets/Product10.png"
+import Product11 from "../Assets/Product11.png"
+import Product12 from "../Assets/Product12.png"
+import Product13 from "../Assets/Product13.png"
+import Product14 from "../Assets/Product14.png"
+import Product15 from "../Assets/Product15.png"
 
 export const products = [
     {
@@ -34,5 +44,70 @@ export const products = [
         Price : "₱2,799.00",
         ImagePath: Product5
 
+    },
+    {
+        name: "Nebula Noir",
+        Price : "₱2,199.00",
+        ImagePath: Product6
+
+    },
+    {
+        name: "Quantum Essence",
+        Price : "₱3,499.00",
+        ImagePath: Product7
+
+    },
+    {
+        name: "Rogue Horizon",
+        Price : "₱2,999.00",
+        ImagePath: Product8
+
+    },
+    {
+        name: "Atlas Odyssey",
+        Price : "₱2,799.00",
+        ImagePath: Product9
+
+    },
+    {
+        name: "Midnight Maverick",
+        Price : "₱3,699.00",
+        ImagePath: Product10
+
+    },
+    {
+        name: "Elysian Echo",
+        Price : "₱3,999.00",
+        ImagePath: Product11
+
+    },
+    {
+        name: "Celestial Serenity",
+        Price : "₱2,899.00",
+        ImagePath: Product12
+
+    },
+    {
+        name: "Siren's Symphony",
+        Price : "₱2,699.00",
+        ImagePath: Product13
+
+    },
+    {
+        name: "Enchanté Elysium",
+        Price : "₱2,399.00",
+        ImagePath: Product14
+
+    },
+    {
+        name: "Radiant Reverie",
+        Price : "₱2,999.00",
+        ImagePath: Product15
+
     }
+
+
+
+
+    
 ]
